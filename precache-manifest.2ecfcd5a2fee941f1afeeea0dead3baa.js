32f7bac7faf9ab64c0642ed1a8f7d42e3f2557b9
David Menor <damenor.develop@gmail.com>
2019-10-08 01:31:16 +0200

    Updates

---
+++ precache-manifest.2ecfcd5a2fee941f1afeeea0dead3baa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c8d0928e87f35effe87e8ca1315380b",
+    "url": "/breaking-bad-quotes/index.html"
+  },
+  {
+    "revision": "939c9479e2e48a0fb007",
+    "url": "/breaking-bad-quotes/static/css/main.ed027ae5.chunk.css"
+  },
+  {
+    "revision": "a8e32d0af611c2e766e4",
+    "url": "/breaking-bad-quotes/static/js/2.3fa03b60.chunk.js"
+  },
+  {
+    "revision": "939c9479e2e48a0fb007",
+    "url": "/breaking-bad-quotes/static/js/main.685bde27.chunk.js"
+  },
+  {
+    "revision": "bb7fc2a42e721f3c6442",
+    "url": "/breaking-bad-quotes/static/js/runtime-main.95e67f1e.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/breaking-bad-quotes/static/media/logo.427a4adc.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/breaking-bad-quotes/precache-manifest.2ecfcd5a2fee941f1afeeea0dead3baa.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/breaking-bad-quotes/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
